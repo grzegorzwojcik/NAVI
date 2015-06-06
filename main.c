a894b5e7081e9ed716e4b1bfc45292fbbabdb011
torcek <grzegorz.wojcik91@gmail.com>
2015-06-06 15:26:12 +0200

    New directory

---
+++ main.c
@@ -0,0 +1,69 @@
+/**
+*****************************************************************************
+**
+**  File        : main.c
+**
+**  Abstract    : Quadcopter navigation board main function.
+**
+**  Author		: Grzegorz WÓJCIK
+**
+**  Environment : Atollic TrueSTUDIO/STM32
+**
+*****************************************************************************
+*/
+
+/* Includes */
+#include <stddef.h>
+#include <stdlib.h>
+#include "stm32f10x.h"
+
+/* Private typedef */
+/* Private define  */
+/* Private macro */
+/* Private variables */
+ USART_InitTypeDef USART_InitStructure;
+
+/* Private function prototypes */
+/* Private functions */
+
+/**
+**===========================================================================
+**
+**  Abstract: main program
+**
+**===========================================================================
+*/
+int main(void)
+	{
+
+	int i = 0;
+
+	/* TODO - Add your application code here */
+	RCC_ClocksTypeDef ClksFreq;
+	RCC_GetClocksFreq(&ClksFreq);
+
+	/* Infinite loop */
+	while (1)
+	{
+		i++;
+	}
+}
+
+
+
+/*
+ * Minimal __assert_func used by the assert() macro
+ * */
+void __assert_func(const char *file, int line, const char *func, const char *failedexpr)
+{
+  while(1)
+  {}
+}
+
+/*
+ * Minimal __assert() uses __assert__func()
+ * */
+void __assert(const char *file, int line, const char *failedexpr)
+{
+   __assert_func (file, line, NULL, failedexpr);
+}
