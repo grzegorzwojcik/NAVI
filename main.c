6d5d567b0dba2d666146c9bba17bf56433ddea3f
torcek <grzegorz.wojcik91@gmail.com>
2015-06-06 22:41:58 +0200

    Trying to setup TIM3

--- main.c
+++ main.c
@@ -46,6 +46,18 @@ int main(void)
 	FAULTS_Servo_initTIM();
 	TIM2->CCR3 = 1500;
 
+
+	RCC_APB2PeriphClockCmd( RCC_APB2Periph_GPIOC, ENABLE );
+	GPIO_InitTypeDef GPIO_InitStruct;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_7;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOC, &GPIO_InitStruct);
+	GPIO_PinRemapConfig(GPIO_FullRemap_TIM3, ENABLE);
+
+
+
+
 		while (1)
 		{
 
