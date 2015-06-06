7982482cf4ead311d6ca48860533c02fb6cf4d67
torcek <grzegorz.wojcik91@gmail.com>
2015-06-06 19:54:51 +0200

    PWM does not work yet

--- FAULTS.h
+++ FAULTS.h
@@ -8,6 +8,16 @@
 #ifndef FAULTS_H_
 #define FAULTS_H_
 
-FAULTS_Servo_initGPIO();
+/*** PREDEFINED SETTINGS	***/
+#define SERVO_RCC_APB2Periph	RCC_APB2Periph_GPIOB
+#define SERVO_GPIO_PORT 		GPIOB
+#define SERVO_PIN				GPIO_Pin_10
+
+#define SERVO_RCC_APB1Periph	RCC_APB1Periph_TIM2
+#define SERVO_TIMER				TIM2			/*	Timer with PWM	*/
+
+
+void FAULTS_Servo_initGPIO();
+void FAULTS_Servo_initTIM();
 
 #endif /* FAULTS_H_ */
--- main.c
+++ main.c
@@ -18,6 +18,7 @@
 #include "stm32f10x.h"
 
 #include "functions.h"
+#include "FAULTS.h"
 
 /* Private typedef */
  USART_InitTypeDef USART_InitStructure;
@@ -33,11 +34,17 @@ int main(void)
 
 
 	/* TODO - Add your application code here */
+	//HSEStartUpStatus = RCC_WaitForHSEStartUp();
 	SYSTEM_ClockCheck();
 	SysTick_Config(SystemCoreClock/1000);	// SysTick 1 kHZ (1ms interval)
 
+
+
 	/* Enter infinite loop only when clock frequencies are OK */
 	if( GV_SystemStatus == 1 ){
+	FAULTS_Servo_initGPIO();
+	FAULTS_Servo_initTIM();
+	TIM2->CCR3 = 1500;
 
 		while (1)
 		{
