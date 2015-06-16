dfea23b00d0db7d5fcb0e6bca26bd4417bb6e093
torcek <grzegorz.wojcik91@gmail.com>
2015-06-16 15:28:06 +0200

    SPI.c SPI.h files added, ready to write and test SPI initialization functions

---
+++ SPI.c
@@ -0,0 +1,25 @@
+/*
+ * SPI.c
+ *
+ *  Created on: Jun 16, 2015
+ *      Author: Grzegorz WÓJCIK
+ */
+
+#include <stddef.h>
+#include <stdlib.h>
+#include "stm32f10x.h"
+
+#include "SPI.h"
+
+
+void SPI_initRCC(void){
+
+}
+
+void SPI_initGPIO(void){
+
+}
+
+void SPI_initSPI(void){
+
+}
---
+++ SPI.h
@@ -0,0 +1,26 @@
+/*
+ * SPI.h
+ *
+ *  Created on: Jun 16, 2015
+ *      Author: Grzegorz WÓJCIK
+ */
+
+#ifndef SPI_H_
+#define SPI_H_
+
+/* Predefined settings for SD card */
+#define SD_NSS		GPIO_Pin_4
+#define SD_SCK		GPIO_Pin_5
+#define SD_MISO		GPIO_Pin_6
+#define SD_MOSI		GPIO_Pin_7
+#define	SD_GPIO1	GPIOA
+
+#define SD_WP		GPIO_Pin_4
+#define SD_CD		GPIO_Pin_5
+#define	SD_GPIO2	GPIOC
+
+void SPI_initRCC(void);
+void SPI_initGPIO(void);
+void SPI_initSPI(void);
+
+#endif /* SPI_H_ */
